f01cabf8779a5bd8c47893ad4a90e31486ac5c23
sdenisse <stijn.denissen@vub.be>
2023-03-23 20:09:43 +0100

    Adapt script to work more easily with BIDS dataset

--- test.py
+++ test.py
@@ -18,10 +18,10 @@ from sklearn.metrics import mean_absolute_error
 if __name__ == "__main__":
     # Define command line options
     parser = argparse.ArgumentParser()
+    parser.add_argument('--root_path', type=str, help='Path to parent directory containing subjects subdirectories')
+    parser.add_argument('--phenotypic_tsv_path', type=str, help='Specify path to phenotypic data')
     parser.add_argument('--state_dict_path', type=str, help='Specify path to state_dict')
-    parser.add_argument('--test_df_path', type=str, required=True, help='Specify path to state_dict if desired')
     parser.add_argument('--column_name_id', type=str, default='subject_id', help='Column name of the id column')
-    parser.add_argument('--column_name_img_path', type=str, default='img_path', help='Column name of image path column')
     parser.add_argument('--column_name_label', type=str, default='subject_age', help='Column name of the label column')
     parser.add_argument('--output_path_tsv', type=str, default=None, help='Absolute path to output file (TSV)')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size for the data loaders')
@@ -33,8 +33,9 @@ if __name__ == "__main__":
     criterion = nn.L1Loss()
 
     # Get data loader
-    test_df = pd.read_csv(args.test_df_path, sep='\t')
-    colnames_dict = {'id': args.column_name_id, 'img_path': args.column_name_img_path, 'label': args.column_name_label}
+    test_df = pd.read_csv(args.phenotypic_tsv_path, sep='\t')
+    test_df['img_path'] = test_df[args.column_name_id].apply(lambda x: f'{args.root_path}/{x}/anat/{x}_T1w.nii.gz')
+    colnames_dict = {'id': args.column_name_id, 'img_path': 'img_path', 'label': args.column_name_label}
     test_loader = get_data_loader(test_df, 'test', colnames_dict, batch_size=args.batch_size)
 
     # Load network
