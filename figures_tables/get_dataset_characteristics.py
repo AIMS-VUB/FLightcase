bfc66e44e4dd21a9deef64fcc4213da9c296112f
sdenisse <stijn.denissen@vub.be>
2023-03-24 10:48:19 +0100

    Allow specifying no column names for either or both of the data description methods

--- figures_tables/get_dataset_characteristics.py
+++ figures_tables/get_dataset_characteristics.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_to_tsv', type=str, help='Path to tsv file')
     parser.add_argument('--colname_id', type=str, help='Column name of ID')
-    parser.add_argument('--colnames_describe_method', nargs='+', help='List the column names to assess with the '
+    parser.add_argument('--colnames_describe_method', nargs='*', help='List the column names to assess with the '
                                                                       '.describe() method')
-    parser.add_argument('--colnames_value_counts_method', nargs='+', help='List the column names to assess with the '
+    parser.add_argument('--colnames_value_counts_method', nargs='*', help='List the column names to assess with the '
                                                                           '.value_counts() method')
     parser.add_argument('--settings_dict_path', type=str, help='Path to settings dict used for federated learning')
     parser.add_argument('--output_path_txt', type=str, help='Path to output txt file')
@@ -31,11 +31,13 @@ if __name__ == "__main__":
     # Return .txt file with values
     txt = f'==================\n' \
           f'.describe() method\n' \
-          f'==================\n\n' \
-          f'{df[args.colnames_describe_method].describe()}\n\n' \
-          f'======================\n' \
-          f'.value_counts() method\n' \
-          f'======================\n\n'
+          f'==================\n\n'
+    for col in args.colnames_describe_method:
+        txt += f'{df[col].describe()}\n\n'
+
+    txt += f'======================\n' \
+           f'.value_counts() method\n' \
+           f'======================\n\n'
     for col in args.colnames_value_counts_method:
         txt += f'{df[col].value_counts()}\n\n'
 
