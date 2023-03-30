0e01606c32df6fe4bfd783ad84c1aeca8c120974
sdenisse <stijn.denissen@vub.be>
2023-03-30 12:23:26 +0200

    Remove example of Xavier weight initialisation

--- DL_utils/model.py
+++ DL_utils/model.py
@@ -4,10 +4,9 @@ Utilities related to the model
 """
 
 import torch
-import torch.nn as nn
 
 
-def get_weights(net, state_dict_path=None):
+def get_weights(net, state_dict_path):
     """ Assign weights to a network architecture
 
     :param net: torch network architecture
@@ -15,19 +14,7 @@ def get_weights(net, state_dict_path=None):
     :return: torch network with weights
     """
     # Assign weights to net
-    if state_dict_path is not None:
-        state_dict = torch.load(state_dict_path, map_location='cpu')  # Load state dict
-        net.load_state_dict(state_dict)
-    else:
-        net.apply(init_weights)
+    state_dict = torch.load(state_dict_path, map_location='cpu')  # Load state dict
+    net.load_state_dict(state_dict)
 
     return net
-
-
-def init_weights(m):
-    """
-    Function source: https://stackoverflow.com/questions/49433936/how-do-i-initialize-weights-in-pytorch
-    """
-    if isinstance(m, nn.Linear):
-        torch.nn.init.xavier_uniform(m.weight)
-        m.bias.data.fill_(0.01)
