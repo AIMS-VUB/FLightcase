0d4f1b09411f23b593e1d41e166440b91355879f
sdenisse <stijn.denissen@vub.be>
2023-04-01 18:32:18 +0200

    Copy net before assigning weights

--- DL_utils/model.py
+++ DL_utils/model.py
@@ -3,7 +3,17 @@ Utilities related to the model
 ==> Inspired by the GitHub repository of Wood et al. 2022 (https://github.com/MIDIconsortium/BrainAge)
 """
 
+import copy
 import torch
+
+
+def copy_net(net):
+    """ Copy torch network
+
+    Source: https://androidkt.com/copy-pytorch-model-using-deepcopy-and-state_dict/
+    """
+    net_copy = copy.deepcopy(net)
+    return net_copy
 
 
 def get_weights(net, state_dict_path):
@@ -13,8 +23,11 @@ def get_weights(net, state_dict_path):
     :param state_dict_path: str, path to state dict
     :return: torch network with weights
     """
+
+    copied_net = copy_net(net)
+
     # Assign weights to net
     state_dict = torch.load(state_dict_path, map_location='cpu')  # Load state dict
-    net.load_state_dict(state_dict)
+    copied_net.load_state_dict(state_dict)
 
-    return net
+    return copied_net
