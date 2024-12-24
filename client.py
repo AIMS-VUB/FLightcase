8397f04964c4256b28418265be9b0a344f32d225
sdenisse <stijn.denissen@vub.be>
2024-12-24 13:12:18 +0100

    Save overall_train and test df to .tsv file instead of .csv

--- client.py
+++ client.py
@@ -144,8 +144,9 @@ if __name__ == "__main__":
                                                    train_test_random_state=42, train_val_random_state=random_state)
             if fl_round == 1 and split_i == 0:  # fl_round starts from 1
                 train_overall_df = pd.concat([train_df, val_df], ignore_index=True)
-                train_overall_df.to_csv(os.path.join(workspace_path_client, 'train_overall_df.csv'), index=False)
-                test_df.to_csv(os.path.join(workspace_path_client, 'test_df.csv'), index=False)
+                train_overall_df.to_csv(os.path.join(workspace_path_client, 'train_overall_df.tsv'), sep='\t',
+                                        index=False)
+                test_df.to_csv(os.path.join(workspace_path_client, 'test_df.tsv'), sep='\t', index=False)
             train_loader, n_train = get_data_loader(train_df, 'train', colnames_dict, batch_size, return_n=True)
             val_loader, n_val = get_data_loader(val_df, 'validation', colnames_dict, batch_size, return_n=True)
             test_loader, n_test = get_data_loader(test_df, 'test', colnames_dict, batch_size, return_n=True)
