b7e6a19a9007347d1127f53d4c0f012172d8b8b5
sdenisse <stijn.denissen@vub.be>
2024-12-26 12:50:56 +0100

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+Setup.py file
+
+Sources:
+- https://docs.python.org/3.11/distutils/setupscript.html
+- https://stackoverflow.com/questions/57089252/create-python-development-environment-virtualenv-using-setup-py
+"""
+
+import os
+import pathlib
+from setuptools import setup
+
+parent_dir_path = str(pathlib.Path(__file__).parent.resolve())
+with open(os.path.join(parent_dir_path, 'requirements.txt')) as f:
+    requirements = f.readlines()
+
+setup(
+    name='FLightcase',
+    version='0.1.0',
+    author='Stijn Denissen',
+    author_email='stijn.denissen@vub.be',
+    description='FLightcase toolbox for Federated Learning',
+    url='https://github.com/AIMS-VUB/FLightcase/',
+    license='GPLv3',
+    install_requires=requirements
+)
