afaa8194821cece561edce76c0cf1121868df567
sdenissen <stijn.denissen@vub.be>
2025-01-09 18:08:41 +0100

    Change dependency management

--- setup.py
+++ setup.py
@@ -6,13 +6,7 @@ Sources:
 - https://stackoverflow.com/questions/57089252/create-python-development-environment-virtualenv-using-setup-py
 """
 
-import os
-import pathlib
 from setuptools import setup
-
-parent_dir_path = str(pathlib.Path(__file__).parent.resolve())
-with open(os.path.join(parent_dir_path, 'requirements.txt')) as f:
-    requirements = f.readlines()
 
 setup(
     name='FLightcase',
@@ -22,7 +16,20 @@ setup(
     description='FLightcase toolbox for Federated Learning',
     url='https://github.com/AIMS-VUB/FLightcase/',
     license='GPLv3',
-    install_requires=requirements,
+    install_requires=[
+        'torch==2.5.1',
+        'pandas==2.2.3',
+        'monai==1.4.0',
+        'scikit-learn==1.6.0',
+        'tqdm==4.67.1',
+        'nibabel==5.3.2',
+        'paramiko==3.5.0',
+        'scp==0.15.0',
+        'matplotlib==3.10.0',
+        'scipy==1.14.1',
+        'numpy==1.26.4',  # 2.2.0 causes conflict with monai==1.4.0
+        'click==8.1.8'
+    ],
     entry_points={
         'console_scripts': [
             'FLightcase = cli:cli',
